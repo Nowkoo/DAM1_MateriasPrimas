22b3a988ab51a532cd5a856d8c29393619aebeac
alu10183093 <magoga2896@gmail.com>
2024-05-22 19:55:03 +0200

    arreglos varios

--- src/Model/LeerCSV.java
+++ src/Model/LeerCSV.java
@@ -9,35 +9,7 @@ import java.util.HashMap;
 
 
 public class LeerCSV {
-
     public static ArrayList<MateriaPrima> materiasPrimas = new ArrayList<>();
-    public static void main(String[] args) {
-
-        cargarDatosCSV();
-
-        HashMap<String, Float> componentesUsuarios = new HashMap<>();
-
-        componentesUsuarios.put("sio2",63f);
-        componentesUsuarios.put("al2o3",24f);
-        componentesUsuarios.put("fe2o3",1.2f);
-        componentesUsuarios.put("cao",0.8f);
-        componentesUsuarios.put("mgo",0.4f);
-        componentesUsuarios.put("na2o",0.4f);
-        componentesUsuarios.put("k2o",2.6f);
-        componentesUsuarios.put("tio2",1.3f);
-        componentesUsuarios.put("ppc",6.8f);
-
-
-        for (int i = 0; i < materiasPrimas.size(); i++) {
-
-
-            if (materiasPrimas.get(i).esMateriaPrima(componentesUsuarios)){
-                System.out.println("True");
-                return;
-            }
-        }
-        System.out.println("False");
-    }
 
     public static void cargarDatosCSV(){
 
@@ -75,11 +47,9 @@ public class LeerCSV {
             System.out.println(e.getMessage());
 
         }
-
-
-
-
     }
 
-
+    public static ArrayList<MateriaPrima> getMateriasPrimas() {
+        return materiasPrimas;
+    }
 }
